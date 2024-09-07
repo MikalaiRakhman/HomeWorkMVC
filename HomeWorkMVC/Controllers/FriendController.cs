d9a7856bd0808a62442f917ad1b32e3bd9942842
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-07 18:53:52 +0200

    Added FriendController

---
+++ HomeWorkMVC/Controllers/FriendController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HomeWorkMVC.Controllers
+{
+    public class FriendController : Controller
+    {
+        public IActionResult Index() // /friends
+        {
+            return View();
+        }
+    }
+}
---
+++ HomeWorkMVC/Models/Friend.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeWorkMVC.Models
+{
+    public class Friend
+    {
+        public int FriendID { get; set; }
+        public string FriendName { get; set; }
+        public string Place { get; set; }
+    }
+}
