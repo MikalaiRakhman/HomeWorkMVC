6c1924121acc2f8c3791e3da53ca3e6a9208394f
MikalaiRakhman <rakhmanmikalai@gmail.com>
2024-09-10 14:04:55 +0200

    added methods Create, Delete, Update added views Create, Delete, Update

--- HomeWorkMVC/Controllers/FriendController.cs
+++ HomeWorkMVC/Controllers/FriendController.cs
@@ -8,5 +8,21 @@ namespace HomeWorkMVC.Controllers
         {
             return View();
         }
+
+        public IActionResult Create()
+        {
+	        return View();
+        }
+
+        public IActionResult Update()
+        {
+	        return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
     }
 }
